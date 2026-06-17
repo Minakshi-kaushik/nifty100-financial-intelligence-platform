ed3af9ede1788c847af4f87aa220285c6ce1843d
Minakshi Kaushik <minakshikaushik22122005@gmail.com>
2026-06-17 18:53:24 +0530

    Sprint 1 Day 03 - ETL normaliser, validation framework and manual review

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-companies = pd.read_excel("data/raw/companies.xlsx", header=1)
-
-print(companies["id"].tail(20))
-
-print(companies.shape)
--- test.py
+++ test.py
@@ -2,6 +2,14 @@ import pandas as pd
 
 companies = pd.read_excel("data/raw/companies.xlsx", header=1)
 
-print(companies["id"].tail(20))
-
-print(companies.shape)
+for ticker in [
+    "ULTRACEMCO",
+    "UNIONBANK",
+    "UNITDSPR",
+    "VBL",
+    "VEDL",
+    "WIPRO",
+    "ZOMATO",
+    "ZYDUSLIFE",
+]:
+    print(ticker, ticker in set(companies["id"]))
